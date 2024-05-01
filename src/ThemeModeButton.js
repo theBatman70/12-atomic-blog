889db538c0909cac9428d0157b8a07780011ce53
theBatman70 <russelbajracharya@gmail.com>
2024-05-01 13:38:48 +0545

    refactored using contextAPI

---
+++ src/ThemeModeButton.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+function ThemeModeButton() {
+  const [isFakeDark, setIsFakeDark] = useState(false);
+
+  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
+  useEffect(
+    function () {
+      document.documentElement.classList.toggle("fake-dark-mode");
+    },
+    [isFakeDark]
+    
+  );
+  return (
+    <button
+      onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+      className="btn-fake-dark-mode"
+    >
+      {isFakeDark ? "☀️" : "🌙"}
+    </button>
+  );
+}
+
+export default ThemeModeButton;
